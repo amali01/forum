41466ed8f885461e352b0db0f0e4dbff2fcb13f7
Mohammed AlSammak <msk170a@gmail.com>
2023-11-05 03:55:11 +0300

    get username func

--- api/session.go
+++ api/session.go
@@ -6,16 +6,21 @@ import (
 
 // this map stores the users sessions. For larger scale applications, you can use a database or cache for this purpose
 var (
-	sessions = map[string]session{}
+	Sessions = make(map[string]Session, 0)
 )
 
 // each session contains the username of the user and the time at which it expires
-type session struct {
-	username string
-	expiry   time.Time
+type Session struct {
+	username    string
+	SessionUUID string
+	expiry      time.Time
 }
 
 // we'll use this method later to determine if the session has expired
-func (s session) isExpired() bool {
+func (s Session) IsExpired() bool {
 	return s.expiry.Before(time.Now())
+}
+
+func (s Session) Get_Username() string {
+	return s.username
 }
