779d036d71b99f60ed697258b09aff64f4bc07bd
Mohammed AlSammak <msk170a@gmail.com>
2023-11-10 16:39:58 +0300

    validateuser function now returns the session and bool is valid or not

--- api/validateUser.go
+++ api/validateUser.go
@@ -5,17 +5,17 @@ import (
 	"net/http"
 )
 
-func ValidateUser(w http.ResponseWriter, r *http.Request) {
+func ValidateUser(w http.ResponseWriter, r *http.Request) (*Session, bool) {
 	cookie, err := r.Cookie("session_token")
 	if err != nil {
 		if err == http.ErrNoCookie {
 			// If the cookie is not set, return an unauthorized status
 			w.WriteHeader(http.StatusUnauthorized)
-			return
+			return nil, false
 		}
 		// For any other type of error, return a bad request status
 		w.WriteHeader(http.StatusBadRequest)
-		return
+		return nil, false
 	}
 
 	// Get cookie value
@@ -26,7 +26,7 @@ func ValidateUser(w http.ResponseWriter, r *http.Request) {
 	if !exists {
 		// If the session token is not present in session map, return an unauthorized error
 		w.WriteHeader(http.StatusUnauthorized)
-		return
+		return nil, false
 	}
 
 	// If the session is present, but has expired, we can delete the session, and return
@@ -34,9 +34,10 @@ func ValidateUser(w http.ResponseWriter, r *http.Request) {
 	if userSession.IsExpired() {
 		delete(Sessions, session_token)
 		w.WriteHeader(http.StatusUnauthorized)
-		return
+		return nil, false
 	}
 
 	// If the session is valid, return a welcome message to the user
-	fmt.Fprintf(w, "Welcome %s!", userSession.Get_Username())
+	fmt.Fprintf(w, "Welcome %d!", userSession.Get_UserID())
+	return &userSession, true // return the session
 }
