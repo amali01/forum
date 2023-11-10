620dbdd7e73bcd47eed0c40d96bd2b8b8af9714d
Mohammed AlSammak <msk170a@gmail.com>
2023-11-10 20:31:12 +0300

    create post method

--- api/create_post.go
+++ api/create_post.go
@@ -15,13 +15,17 @@ type Post struct {
 }
 
 func Create_Post(w http.ResponseWriter, r *http.Request) {
+	// Handling only POST method requests
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
 	userSession, valid := ValidateUser(w, r)
 
 	if valid == false {
 		w.Write([]byte("Unauthorize access"))
 		return
 	}
-
 	// Read the request body
 	body, err := io.ReadAll(r.Body)
 	if err != nil {
@@ -31,14 +35,20 @@ func Create_Post(w http.ResponseWriter, r *http.Request) {
 
 	var data Post
 
-	// Unmarshal the JSON data from the request body
+	// Unmarshal the JSON data from the request body into 'data' variable of type Post
 	if err := json.Unmarshal(body, &data); err != nil {
 		http.Error(w, "Failed to unmarshal JSON", http.StatusBadRequest)
 		return
 	}
-	fmt.Println(data)
 
-	funcs.CreatePost(userSession.Get_UserID(), data.Category, data.Post)
+	////////////////////////////////////////////////////////////////////////
 
+	err = funcs.CreatePost(userSession.Get_UserID(), data.Category, data.Post)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("%s", err), http.StatusBadRequest)
+
+	}
 	w.Write([]byte("OK!"))
+	////////////////////////////////////////////////////////////////////////
+
 }
