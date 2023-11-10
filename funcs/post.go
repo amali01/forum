fd57a82337ca240c545e804edc70ff48cdc77d8d
Mohammed AlSammak <msk170a@gmail.com>
2023-11-10 20:32:31 +0300

    CreatePost function

--- funcs/post.go
+++ funcs/post.go
@@ -5,21 +5,41 @@ import (
 	"strings"
 )
 
-// didn't try it yet
 func CreatePost(userID int, category string, content string) error {
-	catId, err := GetCategoryID(category)
+	// Fetching Category ID
+	catID, err := GetCategoryID(category)
 	if err != nil {
-		return err
+		return fmt.Errorf("Category does not exist")
 	}
+
+	// Trimming whitespace from the content
 	content = strings.TrimSpace(content)
 	if content == "" {
-		return fmt.Errorf("message cannot be empty")
+		return fmt.Errorf("Message cannot be empty")
 	}
-	query := "INSERT INTO posts (user_id, category, post) VALUES (?, ?, ?)"
-	if _, err := DB.Exec(query, userID, catId, content); err != nil {
-		return err
+
+	//////////////////////////////////////////////////////////////////
+
+	// Inserting post data into the database
+	query := "INSERT INTO posts (user_id, post) VALUES (?, ?)"
+	result, err := DB.Exec(query, userID, content)
+	if err != nil {
+		return fmt.Errorf("Failed to insert the post")
+	}
+
+	lastID, err := result.LastInsertId()
+	if err != nil {
+		return fmt.Errorf("Failed to retrieve the last inserted ID")
+	}
+	postID := int(lastID)
+
+	// Inserting the post category into the database
+	query = "INSERT INTO threads (post_id, cat_id) VALUES (?, ?)"
+	if _, err := DB.Exec(query, postID, catID); err != nil {
+		return fmt.Errorf("Failed to insert the post category")
 	}
 	return nil
+
 }
 
 func GetCategoryID(category string) (int, error) {
