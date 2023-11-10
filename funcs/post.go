7efcc8516e5f3c909e7949f2233e336977390c2c
Mohammed AlSammak <msk170a@gmail.com>
2023-11-10 21:04:05 +0300

    add category to database

--- funcs/post.go
+++ funcs/post.go
@@ -57,6 +57,25 @@ func CreateCategory(userId int, categoryName string) error {
 	if !UserIsType(userId, "admin") {
 		return fmt.Errorf("only admins can create categories")
 	}
+	// Trimming whitespace from the categoryName
+	categoryName = strings.TrimSpace(categoryName)
+	if categoryName == "" {
+		return fmt.Errorf("Category cannot be empty")
+	}
+
+	catID, err := GetCategoryID(categoryName)
+	if err != nil {
+		// Inserting category data into the database
+		query := "INSERT INTO category (category) VALUES (?)"
+		if _, err := DB.Exec(query, categoryName); err != nil {
+			return fmt.Errorf("Failed to insert the category")
+		}
+		return nil
+	}
+	if catID != 0 {
+		return fmt.Errorf("This Category already exist")
+	}
+	///////////////////////////////////////////////////////////////////////////
 	query := "INSERT INTO category (category) VALUES (?)"
 	if _, err := DB.Exec(query, categoryName); err != nil {
 		return err
