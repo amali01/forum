2ab7dc12defc1ce47ac784db3dffc0ac6a25f884
Mohammed AlSammak <msk170a@gmail.com>
2023-11-10 20:31:47 +0300

    struct forms

--- api/signup.go
+++ api/signup.go
@@ -39,7 +39,7 @@ func SignUp(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 
-		var data Person
+		var data SignUp_form
 
 		// Unmarshal the JSON data from the request body
 		if err := json.Unmarshal(body, &data); err != nil {
@@ -50,7 +50,7 @@ func SignUp(w http.ResponseWriter, r *http.Request) {
 		// Hash the password before adding it
 		hash, _ := hashing.HashPassword(data.Password) // ignore error for the sake of simplicity
 
-		if funcs.AddUser(data.Name, hash) != nil {
+		if funcs.AddUser(data.Email, hash) != nil {
 			io.WriteString(w, "Add user error, try again")
 			return
 		}
