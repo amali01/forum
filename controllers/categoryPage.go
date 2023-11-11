ec8e30c9e54ad83c7cf33a330eb32cd3afede29e
Mohammed AlSammak <msk170a@gmail.com>
2023-11-11 20:37:18 +0300

    category page server

---
+++ controllers/categoryPage.go
@@ -0,0 +1,40 @@
+package controllers
+
+import (
+	"database/sql"
+	"forum/funcs"
+	"html/template"
+	"net/http"
+	"strings"
+)
+
+// Category page Handler
+func RenderCategoryPage(w http.ResponseWriter, r *http.Request, data *sql.DB) {
+	// Check if the request is not GET && NOT POST requests
+	if r.Method != "GET" {
+		HTTPErrorHandler(w, r, http.StatusMethodNotAllowed)
+		return
+	}
+
+	categoryName := strings.TrimPrefix(r.URL.Path, "/category/")
+	_, err := funcs.GetCategoryID(categoryName)
+	if err != nil {
+		http.Error(w, "This Category do not exist", http.StatusBadRequest)
+		return
+	}
+	/////////////////////////////////////////////////////////////////////////////////////////
+	files := []string{
+		"barebones/mainpage.html", // need to creat a category page
+	}
+
+	tmpl, err := template.ParseFiles(files...)
+	if err != nil {
+		HTTPErrorHandler(w, r, http.StatusInternalServerError)
+		return
+	}
+
+	if err := tmpl.Execute(w, data); err != nil {
+		HTTPErrorHandler(w, r, http.StatusInternalServerError)
+		return
+	}
+}
