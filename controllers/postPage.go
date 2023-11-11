f4a332efa637d271f5d7cd98641aa910e65825d6
Mohammed AlSammak <msk170a@gmail.com>
2023-11-11 20:37:43 +0300

    post page server

---
+++ controllers/postPage.go
@@ -0,0 +1,40 @@
+package controllers
+
+import (
+	"database/sql"
+	"forum/funcs"
+	"html/template"
+	"net/http"
+	"strings"
+)
+
+// Category page Handler
+func RenderPostPage(w http.ResponseWriter, r *http.Request, data *sql.DB) {
+	// Check if the request is not GET && NOT POST requests
+	if r.Method != "GET" {
+		HTTPErrorHandler(w, r, http.StatusMethodNotAllowed)
+		return
+	}
+
+	postID := strings.TrimPrefix(r.URL.Path, "/post/")
+	_, err := funcs.GetPostID(postID)
+	if err != nil {
+		http.Error(w, "This Post do not exist", http.StatusBadRequest)
+		return
+	}
+	/////////////////////////////////////////////////////////////////////////////////////////
+	files := []string{
+		"barebones/postpage.html", // need to creat a post page
+	}
+
+	tmpl, err := template.ParseFiles(files...)
+	if err != nil {
+		HTTPErrorHandler(w, r, http.StatusInternalServerError)
+		return
+	}
+
+	if err := tmpl.Execute(w, data); err != nil {
+		HTTPErrorHandler(w, r, http.StatusInternalServerError)
+		return
+	}
+}
