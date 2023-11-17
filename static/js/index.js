6ce58c82868686f7a1ddb2a00a5a3bec3e5911b2
Mohammed AlSammak <msk170a@gmail.com>
2023-11-17 22:54:38 +0300

    edited post card script

--- static/js/index.js
+++ static/js/index.js
@@ -3,18 +3,37 @@ fetch('/posts')
     .then(response => response.json())
     .then(data => {
         // Process the JSON data and create HTML elements
-        const jsonContainer = document.getElementById('postcardwrapper');
+        const jsonContainer = document.getElementsByClassName('postcardwrapper')[0];
         data.posts.forEach(post => {
             const postElement = document.createElement('div');
             postElement.className = "postcard"
             postElement.innerHTML = `
-               
-                   
-                    <p>Title: ${post.title}</p>
-                    
-               
+            <div class="postWrapper">
+                <div class="postImage"></div>
+                    <div class="dataWrapper">
+                        <div class="data">
+                            <div class="title">
+                                ${post.title} 
+                                <div class="categories"> 
+                                    <div class="category">
+                                        ${post.category}
+                                    </div>
+                            </div>
+                            <div class="user">
+                                <div class="userID">User ID: ${post.user_id}</div>
+                                <div class="action">
+                                    <p>Creation Date: ${post.creation_date}</p>
+                                    <p>{commentsCount}</p>
+                                    <p>{likes count}</p>
+                                </div>
+                            </div>
+                        </div>
+                        </div>
+                    <div>likeICON</div>
+            </div>
             `;
             jsonContainer.appendChild(postElement);
+
         });
     })
     .catch(error => console.error('Error fetching JSON:', error));
