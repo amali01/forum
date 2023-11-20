cac39dcf89387d95b29028424d1997d91d251d29
Mohammed AlSammak <msk170a@gmail.com>
2023-11-20 23:11:04 +0300

    category array

--- pkgs/funcs/struct_prep.go
+++ pkgs/funcs/struct_prep.go
@@ -1,11 +1,11 @@
 package funcs
 
 type Post_json struct {
-	User_ID       string `json:"user_id"`
-	Title         string `json:"title"`
-	Text          string `json:"text"`
-	Category      string `json:"category"`
-	Edited        bool   `json:"edited"`
-	Creation_Date string `json:"creation_date"`
-	Likes_Count   int    `json:"likes_count"`
+	User_ID       string   `json:"user_id"`
+	Title         string   `json:"title"`
+	Text          string   `json:"text"`
+	Category      []string `json:"category"`
+	Edited        bool     `json:"edited"`
+	Creation_Date string   `json:"creation_date"`
+	Likes_Count   int      `json:"likes_count"`
 }
