f25a04d5a0520a9ff903616630bb7cd3f3c2464f
Mohammed AlSammak <msk170a@gmail.com>
2023-11-20 08:03:04 +0300

    post struct (under maintenance)

--- pkgs/funcs/struct_prep.go
+++ pkgs/funcs/struct_prep.go
@@ -1,8 +1,17 @@
 package funcs
 
+type Posts_json struct {
+	User_ID       string `json:"user_id"`
+	Title         string `json:"title"`
+	Category      string `json:"category"`
+	Creation_Date string `json:"creation_date"`
+	Likes_Count   int    `json:"likes_count"`
+}
+
 type Post_json struct {
 	User_ID       string `json:"user_id"`
 	Title         string `json:"title"`
+	Text          string `json:"text"`
 	Category      string `json:"category"`
 	Creation_Date string `json:"creation_date"`
 	Likes_Count   int    `json:"likes_count"`
