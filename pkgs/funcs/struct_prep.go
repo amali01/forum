54cbced92d7b1e4f92225981015e761dad9822ad
Mohammed AlSammak <msk170a@gmail.com>
2023-12-16 14:51:07 +0300

    struct holds user name

--- pkgs/funcs/struct_prep.go
+++ pkgs/funcs/struct_prep.go
@@ -2,6 +2,7 @@ package funcs
 
 type Post_json struct {
 	User_ID       string   `json:"user_id"`
+	User_Name     string   `json:"user_name"`
 	Title         string   `json:"title"`
 	Text          string   `json:"text"`
 	Category      []string `json:"category"`
