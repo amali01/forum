3078b66a21ef54d0c67a12624bc8e1955bfe6b1b
amali <e.amjad2010@gmail.com>
2023-12-17 19:04:55 +0300

    fix cat filter

--- controllers/categoryPage.go
+++ controllers/categoryPage.go
@@ -2,11 +2,16 @@ package controllers
 
 import (
 	"database/sql"
+	"encoding/json"
+	"fmt"
 	"forum/pkgs/funcs"
-	"html/template"
 	"net/http"
 	"strings"
 )
+
+type posts_capsul_json struct {
+	Posts []funcs.Post_json `json:"posts"`
+}
 
 // Category page Handler
 func RenderCategoryPage(w http.ResponseWriter, r *http.Request, data *sql.DB) {
@@ -17,24 +22,34 @@ func RenderCategoryPage(w http.ResponseWriter, r *http.Request, data *sql.DB) {
 	}
 
 	categoryName := strings.TrimPrefix(r.URL.Path, "/category/")
-	_, err := funcs.GetCategoryID(categoryName)
+	catID, err := funcs.GetCategoryID(categoryName)
 	if err != nil {
 		http.Error(w, "This Category do not exist", http.StatusBadRequest)
 		return
 	}
-	/////////////////////////////////////////////////////////////////////////////////////////
-	files := []string{
-		"/static/html/index.html", // need to creat a category page
-	}
-
-	tmpl, err := template.ParseFiles(files...)
+	postIDs, err := funcs.GetCategoryPosts(catID)
 	if err != nil {
-		HTTPErrorHandler(w, r, http.StatusInternalServerError)
+		http.Error(w, "This Category do not exist", http.StatusBadRequest)
 		return
 	}
 
-	if err := tmpl.Execute(w, data); err != nil {
-		HTTPErrorHandler(w, r, http.StatusInternalServerError)
+	/////////////////////////////////////////////////////////////////////////////////////////
+	posts_arr, _ := funcs.Get_posts_of_category(postIDs)
+
+	/* Used to encapsulate the struct into one struct that is used to construct JSON for sending to front-end */
+	JSON_Response := posts_capsul_json{
+		Posts: posts_arr,
+	}
+	/* Marshal the data into JSON format */
+	jsonData, err := json.Marshal(JSON_Response)
+	if err != nil {
+		fmt.Println("Error marshaling JSON:", err)
 		return
 	}
+
+	/* Set the content type to JSON */
+	w.Header().Set("Content-Type", "application/json")
+
+	/* Write the JSON data to the response writer */
+	w.Write(jsonData)
 }
