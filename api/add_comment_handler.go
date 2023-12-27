c264364f80417d6ed5e345ef715ba625a3f8016f
amali <e.amjad2010@gmail.com>
2023-12-27 15:40:33 +0300

    no change just lookes better

--- api/add_comment_handler.go
+++ api/add_comment_handler.go
@@ -15,43 +15,38 @@ type Comment struct {
 
 func AddCommentHandler(w http.ResponseWriter, r *http.Request) {
 	// Handling only POST method requests
-	if r.Method == http.MethodPost {
-		userSession, valid := ValidateUser(w, r)
-
-		if valid == false {
-			w.Write([]byte("Unauthorize access"))
-			return
-		}
-
-		// Read the request body
-		body, err := io.ReadAll(r.Body)
-		if err != nil {
-			http.Error(w, "Failed to read request body", http.StatusInternalServerError)
-			return
-		}
-
-		var data Comment
-
-		// Unmarshal the JSON data from the request body into 'data' variable of type Post
-		if err := json.Unmarshal(body, &data); err != nil {
-			http.Error(w, "Failed to unmarshal JSON", http.StatusBadRequest)
-			return
-		}
-
-		////////////////////////////////////////////////////////////////////////
-
-		err = funcs.CreateComment(userSession.Get_UserID(), data.Post_id, data.Content)
-		if err != nil {
-			http.Error(w, fmt.Sprintf("%s", err), http.StatusBadRequest)
-			return
-		}
-		w.Write([]byte("OK!"))
-		////////////////////////////////////////////////////////////////////////
-
-	} else {
-		// Handle other HTTP methods or incorrect requests
+	if r.Method != http.MethodPost {
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		return
 	}
+	userSession, valid := ValidateUser(w, r)
 
+	if !valid {
+		w.Write([]byte("Unauthorize access"))
+		return
+	}
+
+	// Read the request body
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Failed to read request body", http.StatusInternalServerError)
+		return
+	}
+
+	var data Comment
+
+	// Unmarshal the JSON data from the request body into 'data' variable of type Post
+	if err := json.Unmarshal(body, &data); err != nil {
+		http.Error(w, "Failed to unmarshal JSON", http.StatusBadRequest)
+		return
+	}
+
+	////////////////////////////////////////////////////////////////////////
+
+	err = funcs.CreateComment(userSession.Get_UserID(), data.Post_id, data.Content)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("%s", err), http.StatusBadRequest)
+		return
+	}
+	w.Write([]byte("OK!"))
 }
