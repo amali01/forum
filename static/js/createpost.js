f7f07e4e005017cbab5b0e5c1629d435effdea9c
akhaled <akhaledlarus@gmail.com>
2023-12-29 18:57:55 +0300

    nav bar now comes from js by fething islogged from localstorage

--- static/js/createpost.js
+++ static/js/createpost.js
@@ -1,3 +1,154 @@
+const render_nav_bar = async () => {
+  let nav = ``;
+  let islogged = localStorage.getItem("isloggedIn");
+  if (islogged) {
+    nav += `
+      <nav>
+        <a href="/">
+          <div class="logo">Re4um</div>
+        </a>
+        <ul class="actionitems">
+          <li>
+            <a href="/create_post">
+              <img
+                src="/static/assets/plus-large-svgrepo-com.svg"
+                alt="add Icon"
+                class="navicon"
+                title="Create Post"
+              />
+            </a>
+          </li>
+          <li>
+            <img
+              src="/static/assets/information-circle-svgrepo-com.svg"
+              alt="about Page Icon"
+              class="navicon"
+              title="About"
+            />
+          </li>
+          <li>
+            <img
+              src="/static/assets/API.svg"
+              alt="API Icon"
+              class="navicon"
+              title="API documentation"
+            />
+          </li>
+          <li>
+            <img
+              src="/static/assets/HomeIcon.svg"
+              alt="HomeIcon"
+              class="navicon"
+              title="Back To Homepage"
+              onclick="location.href('/')"
+            />
+          </li>
+          <li>
+            <img
+              src="/static/assets/REgallery.svg"
+              alt="ReGallery (SOON!)"
+              class="navicon"
+              title="Regallery (SOON!)"
+            />
+          </li>
+          <li>
+            <img
+              src="/static/assets/chat.svg"
+              alt="chatIcon"
+              class="navicon"
+              title="chat (SOON)"
+            />
+          </li>
+        </ul>
+        <div>
+          <a href="/logout">
+            <button class="profile" id="profileBtn">
+              Sign Out
+            </button>
+          </a>
+        </div>
+      </nav>
+    `;
+  } else {
+    nav += `
+      <nav>
+        <a href="/">
+          <div class="logo">Re4um</div>
+        </a>
+        <ul class="actionitems">
+          <li>
+            <a href="/create_post">
+              <img
+                src="/static/assets/plus-large-svgrepo-com.svg"
+                alt="add Icon"
+                class="navicon"
+                title="Create Post"
+              />
+            </a>
+          </li>
+          <li>
+            <img
+              src="/static/assets/information-circle-svgrepo-com.svg"
+              alt="about Page Icon"
+              class="navicon"
+              title="About"
+            />
+          </li>
+          <li>
+            <img
+              src="/static/assets/API.svg"
+              alt="API Icon"
+              class="navicon"
+              title="API documentation"
+            />
+          </li>
+          <li>
+            <img
+              src="/static/assets/HomeIcon.svg"
+              alt="HomeIcon"
+              class="navicon"
+              title="Back To Homepage"
+              onclick="location.href('/')"
+            />
+          </li>
+          <li>
+            <img
+              src="/static/assets/REgallery.svg"
+              alt="ReGallery (SOON!)"
+              class="navicon"
+              title="Regallery (SOON!)"
+            />
+          </li>
+          <li>
+            <img
+              src="/static/assets/chat.svg"
+              alt="chatIcon"
+              class="navicon"
+              title="chat (SOON)"
+            />
+          </li>
+        </ul>
+        <div>
+          <a href="/login">
+            <button class="profile" id="profileBtn">
+              Sign In
+            </button>
+          </a>
+          <a href="/signup">
+            <button class="profile" id="profileBtn">
+              Sign Up
+            </button>
+          </a>
+        </div>
+      </nav>
+    `;
+  }
+  let body = document.body;
+  body.insertAdjacentHTML("beforebegin", nav);
+};
+
+document.addEventListener('load', render_nav_bar(), true)
+
 const createPost = async () => {
   event.preventDefault()
   try {
