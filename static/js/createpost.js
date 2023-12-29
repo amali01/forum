e19cbbdd753c0f01322b9085e9723905d0b1f589
akhaled <akhaledlarus@gmail.com>
2023-12-29 17:34:38 +0300

    js now redirects to main index page on success

--- static/js/createpost.js
+++ static/js/createpost.js
@@ -1,26 +1,31 @@
-const formContainer = document.getElementById("FormContainer");
-
 const createPost = async () => {
-  let formData = {
-    Post: document.getElementById("Pcontent").value,
-    Title: document.getElementById("Ptitle").value,
-    Categories: document.getElementById("Pcats").value.split(","),
-  };
-  const response = await fetch("/api/create_post", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(formData),
-  });
-  console.log(response);
-  let t = await response.text;
-  if (t === "OK!") {
-    console.log(formData);
-    //TODO: FIX THIS
-    // location.replace("localhost:8080/");
-  } else {
-    let errdiv = document.getElementById("errdiv");
-    errdiv.innerText = "ERROR CREATING POST";
+  event.preventDefault()
+  try {
+    let formData = {
+      Post: document.getElementById("Pcontent").value,
+      Title: document.getElementById("Ptitle").value,
+      Categories: document.getElementById("Pcats").value.split(","),
+    };
+
+    const response = await fetch("/api/create_post", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    });
+
+    if (response.ok) {
+      console.log("Post created successfully");
+      window.location.replace("/");
+    } else {
+      let errdiv = document.getElementById("errdiv");
+      errdiv.innerText = "ERROR CREATING POST";
+      console.error(
+        `Failed to create post. Server returned ${response.status} status.`
+      );
+    }
+  } catch (error) {
+    console.error("An error occurred while creating the post:", error);
   }
 };
