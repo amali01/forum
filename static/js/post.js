66c006fd30380ebbc02e0789fd0325776a1d9623
Mohammed AlSammak <msk170a@gmail.com>
2024-01-01 20:45:03 +0300

    broken things need to fix, in post page and it children

--- static/js/post.js
+++ static/js/post.js
@@ -1,180 +1,26 @@
+import { loadNav, isloggedIn } from "./components/navbar.js";
+
+// globals
+let gotten_posts = [{}];
+
 const postwrapper = document.getElementById("mPostWrapper");
 const postID = parseInt(
   location.href.match(/post\/[0-9]+/)[0].replace("post/", ""),
 );
 console.log(postID);
 
-const isloggedIn = async () => {
-  let res = await fetch("/api/islogged");
-  let ok = await res.text();
-  console.log(ok);
-  let isSignedIn;
-  if (ok === "1") {
-    isSignedIn = "true";
-  } else {
-    isSignedIn = "false";
-  }
-  localStorage.setItem("isloggedIn", isSignedIn);
-  return isSignedIn;
-};
-
 async function evalLogin(fn) {
   let islogged = await isloggedIn();
   if (islogged === "true") {
-    fn();
+    await fn();
   } else {
     window.location.replace("/login");
   }
 }
-
-const loadNav = () => {
-  let nav = ``;
-  let islogged = localStorage.getItem("isloggedIn");
-  if (islogged === "true") {
-    nav += `
-      <nav>
-        <a href="/">
-          <div class="logo">Re4um</div>
-        </a>
-        <ul class="actionitems">
-          <li>
-            <a href="/create_post">
-              <img
-                src="/static/assets/plus-large-svgrepo-com.svg"
-                alt="add Icon"
-                class="navicon"
-                title="Create Post"
-              />
-            </a>
-          </li>
-          <li>
-            <img
-              src="/static/assets/information-circle-svgrepo-com.svg"
-              alt="about Page Icon"
-              class="navicon"
-              title="About"
-            />
-          </li>
-          <li>
-            <img
-              src="/static/assets/API.svg"
-              alt="API Icon"
-              class="navicon"
-              title="API documentation"
-            />
-          </li>
-          <li>
-            <img
-              src="/static/assets/HomeIcon.svg"
-              alt="HomeIcon"
-              class="navicon"
-              title="Back To Homepage"
-              onclick="location.href('/')"
-            />
-          </li>
-          <li>
-            <img
-              src="/static/assets/REgallery.svg"
-              alt="ReGallery (SOON!)"
-              class="navicon"
-              title="Regallery (SOON!)"
-            />
-          </li>
-          <li>
-            <img
-              src="/static/assets/chat.svg"
-              alt="chatIcon"
-              class="navicon"
-              title="chat (SOON)"
-            />
-          </li>
-        </ul>
-        <div>
-          <a href="/logout">
-            <button class="profile" id="profileBtn">
-              Sign Out
-            </button>
-          </a>
-        </div>
-      </nav>
-    `;
-  } else {
-    nav += `
-      <nav>
-        <a href="/">
-          <div class="logo">Re4um</div>
-        </a>
-        <ul class="actionitems">
-          <li>
-            <a href="/login">
-              <img
-                src="/static/assets/plus-large-svgrepo-com.svg"
-                alt="add Icon"
-                class="navicon"
-                title="Create Post"
-              />
-            </a>
-          </li>
-          <li>
-            <img
-              src="/static/assets/information-circle-svgrepo-com.svg"
-              alt="about Page Icon"
-              class="navicon"
-              title="About"
-            />
-          </li>
-          <li>
-            <img
-              src="/static/assets/API.svg"
-              alt="API Icon"
-              class="navicon"
-              title="API documentation"
-            />
-          </li>
-          <li>
-            <img
-              src="/static/assets/HomeIcon.svg"
-              alt="HomeIcon"
-              class="navicon"
-              title="Back To Homepage"
-              onclick="location.href('/')"
-            />
-          </li>
-          <li>
-            <img
-              src="/static/assets/REgallery.svg"
-              alt="ReGallery (SOON!)"
-              class="navicon"
-              title="Regallery (SOON!)"
-            />
-          </li>
-          <li>
-            <img
-              src="/static/assets/chat.svg"
-              alt="chatIcon"
-              class="navicon"
-              title="chat (SOON)"
-            />
-          </li>
-        </ul>
-        <div>
-          <a href="/login">
-            <button class="profile" id="profileBtn">
-              Sign In
-            </button>
-          </a>
-          <a href="/signup">
-            <button class="profile" id="profileBtn">
-              Sign Up
-            </button>
-          </a>
-        </div>
-      </nav>
-    `;
-  }
-  let body = document.body;
-  body.insertAdjacentHTML("beforebegin", nav);
-};
+// Render navbar
+let nav = loadNav("../../"); // ../../ is to go to home /
+let body = document.body;
+body.insertAdjacentHTML("beforebegin", nav);
 
 const readyPost = async () => {
   let Response = await fetch(`/api/post/${postID}`);
@@ -182,7 +28,6 @@ const readyPost = async () => {
     console.log("ERROR FETCHING DATA");
   }
   let postData = await Response.json();
-
   await orgPostHTML(postwrapper, postData);
 };
 
@@ -207,44 +52,59 @@ const orgPostHTML = async (wrapper, prop) => {
   let comments = await orgComments();
 
   wrapper.innerHTML += `
-            <div class="profilestuff">
-                <div class="pfpImage">
-                    <img src="../../static/assets/reddit.png" alt="reddit lol" class="pimg">
-                </div>
-                <div class="profileinfo">
-                    <div class="profileName">${prop.user_name}</div>
-                    <div class="postinfo">
-                        <div class="postDate">${prop.creation_date}</div>
-                        <div class="commentsLink">comments</div>
-                          <div class="likeBtn" onclick="evalLogin(LikeEvent(0, ${postID}, 'post', 1))">
-                             <img src="../../static/assets/icons8-accept-30.png" alt="like Heart">
-                          </div>
-                          ${prop.post_likes}
-                          <div class="dislikeBtn" onclick="evalLogin(disLikeEvent(0, ${postID}, 'post', -1))">
-                              <img src="../../static/assets/icons8-dislike-30.png" alt="dislike Heart">
-                          </div>
-                          ${prop.post_dislikes}
-                    </div>
-                </div>
-            </div>
-            <div class="posttitle">${prop.title}</div>
-            <div class="postcats">
-                ${cats}
-            </div>
-            <div class="postcontent">
-            ${prop.text}
-            </div>
-            <hr>
-            <div class="commentAnnounce">
-                        Comments
-                    </div>
-            <div class="postcomments">
-                <div class="comment">
-                    ${comments}
-                </div>
-            </div>
+    <div class="profilestuff">
+      <div class="pfpImage">
+        <img src="../../static/assets/reddit.png" alt="reddit lol" class="pimg">
+      </div>
+      <div class="profileinfo">
+        <div class="profileName">${prop.user_name}</div>
+        <div class="postinfo">
+          <div class="postDate">${prop.creation_date}</div>
+          <div class="commentsLink">comments</div>
+          <div class="likeBtn" onclick="LikeEvent(0, ${prop.post_id})">
+            <img src="${
+              prop.isLiked === 1
+                ? " ../../static/assets/icons8-accept-30(1).png"
+                : "../../static/assets/icons8-accept-30.png"
+            }"
+              alt="Like">
           </div>
-        `;
+          <!-- Show like counts -->
+          <div id="likes_${prop.post_id}">
+            ${prop.post_likes}
+          </div>
+          <div class="dislikeBtn" onclick="disLikeEvent(0, ${prop.post_id})">
+            <img src="${
+              prop.isLiked === -1
+                ? " ../../static/assets/icons8-dislike-30(1).png"
+                : "../../static/assets/icons8-dislike-30.png"
+            }"
+              alt="Dislike">
+          </div>
+          <!-- Show like counts -->
+          <div id="dislkes_${prop.post_id}">
+            ${prop.post_dislikes}
+          </div>
+        </div>
+
+      </div>
+    </div>
+    <div class="posttitle">${prop.title}</div>
+    <div class="postcats">
+      ${cats}
+    </div>
+    <div class="postcontent">
+      ${prop.text}
+    </div>
+    <hr>
+    <div class="commentAnnounce">
+      Comments
+    </div>
+    <div class="postcomments">
+      <div class="comment">
+        ${comments}
+      </div>
+    </div>`;
 };
 
 const orgComments = async () => {
@@ -274,11 +134,11 @@ const orgComments = async () => {
                         </div>
                         <div class="commentInfo">
                             <div class="comlikesdislikes">
-                              <div class="likeBtn" onclick="evalLogin(LikeEvent(${i}, ${com.comment_id}, 'comm', 1))">
+                              <div class="likeBtn" onclick="LikeEvent(${i}, ${com.comment_id}, 'comm', 1)">
                                 <img src="../../static/assets/icons8-accept-30.png" alt="like Heart">
                               </div>
                               ${com.comment_likes}
-                              <div class="dislikeBtn" onclick="evalLogin(disLikeEvent(${i}, ${com.comment_id}, 'comm', -1))">
+                              <div class="dislikeBtn" onclick="disLikeEvent(${i}, ${com.comment_id}, 'comm', -1)">
                                 <img src="../../static/assets/icons8-dislike-30.png" alt="dislike Heart">
                               </div>
                               ${com.comment_dislikes}
@@ -293,69 +153,91 @@ const orgComments = async () => {
   return comdiv;
 };
 
-function LikeEvent(index, ID, type, like) {
-  let likeBtn = document.querySelectorAll(".likeBtn")[index];
-  let dislikeBtn = document.querySelectorAll(".dislikeBtn")[index];
+const LikeEvent = async (index, postID) => {
+  let likeBtn = document.querySelectorAll(".likeBtn")[index]; // selects like button
+  let dislikeBtn = document.querySelectorAll(".dislikeBtn")[index]; // selects dislike button
+  let like_count_area = document.getElementById(`likes_${postID}`); // selects like counts area from dom
+  let dislike_count_area = document.getElementById(`dislikes_${postID}`);
+
+  if (gotten_posts[index].isLiked === 1) {
+    gotten_posts[index].isLiked = 0;
+    likeBtn.classList.remove("liked");
+    likeBtn.innerHTML =
+      '<img src="../../static/assets/icons8-accept-30.png" alt="Like">';
+    await sendReqPost(postID, 0);
+  } else {
+    gotten_posts[index].isLiked = 1;
+    likeBtn.classList.add("liked");
+    likeBtn.innerHTML =
+      '<img src="../../static/assets/icons8-accept-30(1).png" alt="Like">';
+
+    await sendReqPost(postID, 1);
+  }
+
+  if (dislikeBtn.classList.contains("disliked")) {
+    dislikeBtn.classList.remove("disliked");
+    dislikeBtn.innerHTML =
+      '<img src="../../static/assets/icons8-dislike-30.png" alt="Dislike">';
+  }
+  /* fetch new like and dislike count and update the DOM */
+  await get_like_dislike_count(postID).then((likes_dislikes) => {
+    like_count_area.innerHTML = likes_dislikes.interactions.like_count;
+    dislike_count_area.innerHTML = likes_dislikes.interactions.dislike_count;
+  });
+};
+
+const disLikeEvent = async (index, postID) => {
+  let likeBtn = document.querySelectorAll(".likeBtn")[index]; // selects like button
+  let dislikeBtn = document.querySelectorAll(".dislikeBtn")[index]; // selects dislike button
+  let like_count_area = document.getElementById(`likes_${postID}`); // selects like counts area from dom
+  let dislike_count_area = document.getElementById(`dislikes_${postID}`);
+
+  if (gotten_posts[index].isLiked === -1) {
+    gotten_posts[index].isLiked = 0;
+    dislikeBtn.classList.remove("disliked");
+    dislikeBtn.innerHTML =
+      '<img src="../../static/assets/icons8-dislike-30.png" alt="Dislike">';
+    await sendReqPost(postID, 0);
+  } else {
+    gotten_posts[index].isLiked = -1;
+    dislikeBtn.classList.add("disliked");
+    dislikeBtn.innerHTML =
+      '<img src="../../static/assets/icons8-dislike-30(1).png" alt="Dislike">';
+
+    await sendReqPost(postID, -1);
+  }
 
   if (likeBtn.classList.contains("liked")) {
     likeBtn.classList.remove("liked");
     likeBtn.innerHTML =
       '<img src="../../static/assets/icons8-accept-30.png" alt="Like">';
-    if (type === "post") {
-      sendReqPost(ID, 0);
-    } else if (type === "comm") {
-      sendReqComment(ID, 0);
-    }
-  } else {
-    likeBtn.classList.add("liked");
-
-    if (dislikeBtn.classList.contains("disliked")) {
-      dislikeBtn.classList.remove("disliked");
-      dislikeBtn.innerHTML =
-        '<img src="../../static/assets/icons8-dislike-30.png" alt="Dislike">';
-    }
-
-    likeBtn.innerHTML =
-      '<img src="../../static/assets/icons8-accept-30(1).png" alt="Like">';
-    if (type === "post") {
-      sendReqPost(ID, 1);
-    } else if (type === "comm") {
-      sendReqComment(ID, like);
-    }
   }
-}
+  /* fetch new like and dislike count and update the DOM */
+  await get_like_dislike_count(postID).then((likes_dislikes) => {
+    like_count_area.innerHTML = likes_dislikes.interactions.like_count;
+    dislike_count_area.innerHTML = likes_dislikes.interactions.dislike_count;
+  });
+};
 
-function disLikeEvent(index, ID, type, disLike) {
-  let likeBtn = document.querySelectorAll(".likeBtn")[index];
-  let dislikeBtn = document.querySelectorAll(".dislikeBtn")[index];
+const get_like_dislike_count = async (postID) => {
+  let interactions_obj = {};
+  await fetch("../../api/postlikes", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      post_id: postID,
+    },
+  })
+    .then((resp) => {
+      return resp.json();
+    })
+    .then((data) => {
+      interactions_obj = data;
+      console.log(interactions_obj);
+    });
 
-  if (dislikeBtn.classList.contains("disliked")) {
-    dislikeBtn.classList.remove("disliked");
-    dislikeBtn.innerHTML =
-      '<img src="../../static/assets/icons8-dislike-30.png" alt="Dislike"> ';
-    if (type === "post") {
-      sendReqPost(ID, 0);
-    } else if (type === "comm") {
-      sendReqComment(ID, 0);
-    }
-  } else {
-    dislikeBtn.classList.add("disliked");
-
-    if (likeBtn.classList.contains("liked")) {
-      likeBtn.classList.remove("liked");
-      likeBtn.innerHTML =
-        '<img src="../../static/assets/icons8-accept-30.png" alt="Like">';
-    }
-
-    dislikeBtn.innerHTML =
-      '<img src="../../static/assets/icons8-dislike-30(1).png" alt="Dislike">';
-    if (type === "post") {
-      sendReqPost(ID, disLike);
-    } else if (type === "comm") {
-      sendReqComment(ID, disLike);
-    }
-  }
-}
+  return interactions_obj;
+};
 
 const sendRequest = async (url, data) => {
   await fetch(url, {
