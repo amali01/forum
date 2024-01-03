316099205f8ddf3d2ecfb5222e31d9d354b9f057
amali <e.amjad2010@gmail.com>
2024-01-03 19:05:18 +0300

    created Serve_comm_likes_dislikes func and handler

--- api/serve_post_likes_dislikes.go
+++ api/serve_post_likes_dislikes.go
@@ -49,3 +49,41 @@ func Serve_post_likes_dislikes (w http.ResponseWriter, r *http.Request) {
 	w.Write(jsonData)
 
 }
+
+func Serve_comm_likes_dislikes (w http.ResponseWriter, r *http.Request) {
+
+	if r.Method != http.MethodGet {
+	  controllers.HTTPErrorHandler(w, r, http.StatusMethodNotAllowed)
+  
+	  return
+	}
+  
+   // Get the value of the "comm_id" header
+	  commIDStr := r.Header.Get("comm_id")
+  
+	  // Convert the string to an integer
+	  commID, _ := strconv.Atoi(commIDStr)
+	likes_dislikes_count,_ := funcs.CountCommentLikes(commID)
+  
+	// For debug
+	//fmt.Println(likes_dislikes_count)
+  
+  /* Used to encapsulate the struct into one struct that is used to construct JSON for sending to front-end */
+	  JSON_Response := like_dislike_capsul{
+		  Interactions: likes_dislikes_count,
+	  }
+	  /* Marshal the data into JSON format */
+	  jsonData, err := json.Marshal(JSON_Response)
+	  if err != nil {
+		  fmt.Println("Error marshaling JSON:", err)
+		  return
+	  }
+  
+	  /* Set the content type to JSON */
+	  w.Header().Set("Content-Type", "application/json")
+  
+	  /* Write the JSON data to the response writer */
+	  w.Write(jsonData)
+  
+  }
+  
