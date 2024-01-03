060a07f06018e4ff16feee257781f2b8f1afbcf5
Mohammed AlSammak <msk170a@gmail.com>
2024-01-04 02:28:28 +0300

    moved is logged to different file

--- static/js/components/navbar.js
+++ static/js/components/navbar.js
@@ -1,16 +1,4 @@
-const isloggedIn = async () => {
-  let res = await fetch("/api/islogged");
-  let ok = await res.text();
-  /* console.log(ok); */
-  let isSignedIn;
-  if (ok === "1") {
-    isSignedIn = "true";
-  } else {
-    isSignedIn = "false";
-  }
-  localStorage.setItem("isloggedIn", isSignedIn);
-  return isSignedIn;
-};
+import { isloggedIn } from "../helper_funcs/isLoggedIn.js";
 
 const loadNav = async (home_path) => {
   await isloggedIn(); // check if user is logged in
