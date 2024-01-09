d369f26443b5864fe304f195de75fdd6a09874ff
amali <e.amjad2010@gmail.com>
2024-01-09 21:27:46 +0300

    creat Database file if it does not exist

--- pkgs/funcs/db.go
+++ pkgs/funcs/db.go
@@ -2,8 +2,12 @@ package funcs
 
 import (
 	"database/sql"
+	"fmt"
+	"forum/pkgs/hashing"
 	"log"
 	"os"
+
+	"github.com/mattn/go-sqlite3"
 )
 
 const DBPath string = "forum.db"
@@ -14,10 +18,55 @@ func Init() {
 	// connect to the database
 	_, err := os.Stat(DBPath)
 	if os.IsNotExist(err) {
-		log.Fatal("Database file does not exist.")
+		InitateDB(DBPath)
 	}
 	DB, err = sql.Open("sqlite3", DBPath)
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func InitateDB(DBPath string) {
+	DB, err := sql.Open("sqlite3", "forum.db")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = CreateUserTables(DB)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = CreatePostsTables(DB)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = CreateCommentsTables(DB)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func CreateAdmin(DB *sql.DB) error {
+	HashedPassword, _ := hashing.HashPassword("admin")
+	email := "admin"
+	userTypeID, err := UserTypeID("admin")
+	if err != nil {
+		return err
+	}
+
+	query := "INSERT INTO users (user_email, user_pwd, user_type) VALUES (?, ?, ?)"
+	if _, err := DB.Exec(query, email, HashedPassword, userTypeID); err != nil {
+
+		sqliteErr, ok := err.(sqlite3.Error)
+		if ok && sqliteErr.ExtendedCode == sqlite3.ErrConstraintUnique {
+			return fmt.Errorf("user already exist")
+		}
+		return err
+	}
+
+	err = CreateUserProfile(email, email)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return nil
 }
