15c67570a18408131b24b489bb132a4a4ad8b90d
amali <e.amjad2010@gmail.com>
2024-01-10 10:09:41 +0300

    comments limit in frontEnd JS 500 char

--- static/js/createComment.js
+++ static/js/createComment.js
@@ -1,46 +1,13 @@
-// Function to add a new comment
-const addComment = async (commentText) => {
-  try {
-    const response = await fetch("../../api/add_comment", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        post_id: postID,
-        content: commentText,
-      }),
-    });
-
-    if (!response.ok) {
-      console.error("Error adding comment");
-      return;
-    }
-
-    const newComment = await response.json();
-
-    // Update the UI with the newly added comment
-    const commentDiv = document.querySelector(".postcomments .comment");
-    const newCommentHTML = await orgComments([newComment]);
-    commentDiv.innerHTML += newCommentHTML;
-
-
-  } catch (error) {
-    console.error("Error adding comment:", error);
-  }
-};
-
 document.addEventListener("DOMContentLoaded", () => {
   // Handle form submission
   const commentForm = document.getElementById("newCommentForm");
-  commentForm.addEventListener("submit", async(event) => {
+  commentForm.addEventListener("submit", async (event) => {
     event.preventDefault(); // Prevent the default form submission behavior
 
-    
     let islogged = localStorage.getItem("isloggedIn");
     if (islogged === "false") {
-      window.location.replace('/login')
-      return
+      window.location.replace("/login");
+      return;
     }
 
     // Get the comment text from the textarea
@@ -48,21 +15,50 @@ document.addEventListener("DOMContentLoaded", () => {
 
     // Check the length of the comment text
     if (commentText.length > 500) {
-    alert("Comment cannot exceed 500 characters.");
-    return;
+      alert("Comment cannot exceed 500 characters.");
+      return;
     }
 
-    // Call the addComment function with the comment text
-    addComment(commentText);
+    // addComment function with the comment text
+    try {
+      const response = await fetch("../../api/add_comment", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          post_id: postID,
+          content: commentText,
+        }),
+      });
+      let done = 0;
+      if (response.ok) {
+        const newComment = await response.json();
 
-    // reloading the page to show the new comment
-    location.reload();
-    // Clear the textarea after adding the comment
-    document.getElementById("newCommentText").value = "";
+        // Update the UI with the newly added comment
+        const commentDiv = document.querySelector(".postcomments .comment");
+        const newCommentHTML = await orgComments([newComment]);
+        commentDiv.innerHTML += newCommentHTML;
+        done = 1;
+      } else {
+        const errorText = await response.text();
+        alert(errorText);
+        return;
+      }
+    } catch (error) {
+      console.error("Error adding comment:", error);
+    }
+
+    if ((done = 1)) {
+      // reloading the page to show the new comment
+      location.reload();
+      // Clear the textarea after adding the comment
+      document.getElementById("newCommentText").value = "";
+    }
   });
 });
 
 // get the post ID
 const postID = parseInt(
-  location.href.match(/post\/[0-9]+/)[0].replace("post/", ""),
+  location.href.match(/post\/[0-9]+/)[0].replace("post/", "")
 );
